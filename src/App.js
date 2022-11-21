fbf6cba411ba142a12913fcca5b1d107663f04fd
lugDroid <diazbastos@gmail.com>
2022-11-21 09:25:42 +0100

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    axios.get("http://localhost:3001/meals").then((res) => {
+      console.log(res.data);
+    });
+  });
   return (
     <div>
       <h1>Meal Planner</h1>

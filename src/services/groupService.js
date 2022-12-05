ee0ec88475954f1a9b2d5b38296d7da8a016dad5
lugDroid <diazbastos@gmail.com>
2022-12-05 14:22:50 +0100

    Add remaining api operations

--- src/services/groupService.js
+++ src/services/groupService.js
@@ -7,8 +7,26 @@ const getAllGroups = () => {
   return req.then((res) => res.data);
 };
 
+const createGroup = (newGroup) => {
+  const req = axios.post(baseUrl, newGroup);
+  return req.then((res) => res.data);
+};
+
+const deleteGroup = (id) => {
+  const req = axios.delete(`${baseUrl}/${id}`);
+  return req.then((res) => res.data);
+};
+
+const modifyGroup = (id, group) => {
+  const req = axios.put(`${baseUrl}/${id}`, group);
+  return req.then((res) => res.data);
+};
+
 const groupService = {
   getAllGroups,
+  createGroup,
+  deleteGroup,
+  modifyGroup,
 };
 
 export default groupService;

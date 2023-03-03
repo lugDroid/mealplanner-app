a19c1ec99b3e1667ea33c383f551f1d277066b9f
lugDroid <diazbastos@gmail.com>
2023-03-03 16:15:09 +0100

    Add initial code to handle login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ const App = () => {
   const [activeTab, setActiveTab] = useState("meals");
   const [meals, setMeals] = useState([]);
   const [groups, setGroups] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     mealService.getAllMeals().then((initialMeals) => {
@@ -36,9 +38,35 @@ const App = () => {
       tab = <MealsTab />;
   }
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("Loggin in with", userName, password);
+  }
+
   return (
     <div>
       <h1>Meal Planner</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={userName}
+            name="Username"
+            onChange={({ target }) => setUserName(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <button onClick={() => setActiveTab("meals")}>Meals</button>
       <button onClick={() => setActiveTab("groups")}>Groups</button>
       <button onClick={() => setActiveTab("weeklyPlans")}>Weekly Plans</button>
